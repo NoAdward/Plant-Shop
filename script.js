cd4efe441b12c09594201ee4e1516179d76ef34c
NoAdward <dima.tomchuk.99@gmail.com>
2023-02-21 15:07:31 +0200

    burger

---
+++ script.js
@@ -0,0 +1,7 @@
+function navToggle() {
+    var headerAuth = document.getElementById("headerAuth");
+    var menuIcon = document.getElementById("menuIcon");
+    headerAuth.classList.toggle("_active");
+   menuIcon.classList.toggle("_active");
+
+  } 
